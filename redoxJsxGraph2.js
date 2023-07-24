2446fca1508416520c56e166b8542bcbe92e7df9
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2023-07-24 13:23:27 +0300

    Update redoxJsxGraph2.js

--- redoxJsxGraph2.js
+++ redoxJsxGraph2.js
@@ -556,11 +556,10 @@ if (!st){
   }
 }
 });
-var idname="nameinput"+ rqm.toString();
-console.log(idname);
+
 function createSelectDropdown() {
 	
-      let selectTag = {#ox_no_txt#}+': '+ '<select id='+idname+' style="font-family: Arial; font-size: 14px; padding: 5px; border: 1px solid #ccc; border-radius: 4px;>';
+      let selectTag = {#ox_no_txt#}+': '+ '<select id='+"nameinput"+ rqm.toString()+' style="font-family: Arial; font-size: 14px; padding: 5px; border: 1px solid #ccc; border-radius: 4px;>';
       for (let i = minOx-1; i < (maxOx+1); i++) {
 			const sign = Math.sign(i) === 1 ? '+' : '';
         
@@ -578,7 +577,7 @@ const selectTag = createSelectDropdown();
 console.log(selectTag);
 const select = board.create('text', [-3, 4, selectTag], { fixed: true, fontsize:fontsize });
 select.setAttribute({ visible: true });
-const dropdown = document.getElementById(idname);
+const dropdown = document.getElementById("nameinput"+ rqm.toString());
 console.log(dropdown);
 //mouse button event
 p.forEach(function(el, i, p) {
