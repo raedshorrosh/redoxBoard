ec5a17afcf1fe2ede272c27cca7c87178e81cfa8
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2023-07-24 13:44:03 +0300

    Update redoxJsxGraph2.js

--- redoxJsxGraph2.js
+++ redoxJsxGraph2.js
@@ -578,7 +578,7 @@ console.log(selectTag);
 const select = board.create('text', [-3, 4, selectTag], { fixed: true, fontsize:fontsize });
 select.setAttribute({ visible: true });
 
-var bbb=document.getElementById("nameinput");bbb.id+={#rqm#};
+var bbb=document.getElementById("nameinput");bbb.id="nameinput"+{#rqm#};
 const dropdown = document.getElementById(bbb);
 console.log(dropdown);
 //mouse button event
