fac3411f2d0abd925e68e1fd7132bd908a1385b4
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2023-07-25 09:26:04 +0300

    Update redoxJsxGraph2.js

--- redoxJsxGraph2.js
+++ redoxJsxGraph2.js
@@ -559,7 +559,7 @@ if (!st){
 
 function createSelectDropdown() {
 	
-      let selectTag = {#ox_no_txt#}+': '+ '<select id='+"nameinput"+' style="font-family: Arial; font-size: 14px; padding: 5px; border: 1px solid #ccc; border-radius: 4px;>';
+      let selectTag = {#ox_no_txt#}+': '+ '<select id='+"inp"+[#rqm#]+' style="font-family: Arial; font-size: 14px; padding: 5px; border: 1px solid #ccc; border-radius: 4px;>';
       for (let i = minOx-1; i < (maxOx+1); i++) {
 			const sign = Math.sign(i) === 1 ? '+' : '';
         
@@ -577,10 +577,8 @@ const selectTag = createSelectDropdown();
 console.log(selectTag);
 const select = board.create('text', [-3, 4, selectTag], { fixed: true, fontsize:fontsize });
 select.setAttribute({ visible: true });
-
-var bbb=document.getElementById("nameinput");console.log(bbb);
-bbb.id="nameinput"+{#rqm#};
-const dropdown = document.getElementById(bbb.id);console.log(bbb);
+board.update();
+const dropdown = document.getElementById("inp"+[#rqm#]);
 console.log(dropdown);
 //mouse button event
 p.forEach(function(el, i, p) {
